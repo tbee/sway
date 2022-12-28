ca400a56e3d0bed8a1f5afd1bebfd6f98a06a796
Tom Eugelink <tbee@tbee.org>
2022-12-28 17:06:43 +0100

    additional fluent methods

--- src/main/java/org/tbee/sway/SButton.java
+++ src/main/java/org/tbee/sway/SButton.java
@@ -1,6 +1,10 @@
 package org.tbee.sway;
 
-import javax.swing.*;
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
 
 public class SButton extends JButton {
 
@@ -29,6 +33,36 @@ public class SButton extends JButton {
     /** */
     public SButton name(String v) {
         setName(v);
+        return this;
+    }
+
+    public JButton toolTipText(String t) {
+        super.setToolTipText(t);
+        return this;
+    }
+
+    public JButton enabled(boolean v) {
+        super.setEnabled(v);
+        return this;
+    }
+
+    public JButton margin(Insets m) {
+        super.setMargin(m);
+        return this;
+    }
+
+    public JButton actionListener(ActionListener l) {
+        super.addActionListener(l);
+        return this;
+    }
+
+    public JButton icon(Icon i) {
+        super.setIcon(i);
+        return this;
+    }
+
+    public JButton action(Action v) {
+        super.setAction(v);
         return this;
     }
 }
