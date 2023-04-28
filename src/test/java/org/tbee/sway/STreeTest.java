03b188852cf1a844ac0ed9ad9e04aebc052ff868
Tom Eugelink <tbee@tbee.org>
2023-04-28 13:46:26 +0200

    cyclic test

--- src/test/java/org/tbee/sway/STreeTest.java
+++ src/test/java/org/tbee/sway/STreeTest.java
@@ -120,4 +120,64 @@ public class STreeTest extends TestBase {
         // THEN view is updated
         Assertions.assertEquals("Leidscheplein", frameFixture.tree("tree.sTreeCore").valueAt("Amsterdam/Leidscheplein"));
     }
+
+
+    @Test
+    public void selectionModeCyclic() throws Exception {
+
+        // GIVEN
+        City amsterdam = City.of("Amsterdam", 150);
+        City berlin = City.of("Berlin", 560);
+        City rome = City.of("Rome", 1560);
+        City paris = City.of("Paris", 575);
+        amsterdam.addPartnerCity(berlin);
+        berlin.addPartnerCity(rome);
+        rome.addPartnerCity(amsterdam);
+        var cities = List.of(amsterdam, berlin, rome, paris);
+        construct(() -> {
+            sTree = new STree<City>()
+                    .name("tree")
+                    .render(new CityFormat(cities))
+                    .root(amsterdam)
+                    .childrenOf(City::getPartnerCities)
+                    .selectionMode(STree.SelectionMode.MULTIPLE);
+            return TestUtil.inJFrame(sTree, focusMeComponent());
+        });
+
+        // WHEN
+        frameFixture.tree("tree.sTreeCore").clickPath("Amsterdam");
+        // THEN
+        Assertions.assertEquals(1, sTree.getSelection().size());
+        Assertions.assertEquals(amsterdam, sTree.getSelection().get(0));
+
+        // WHEN control select
+        frameFixture.pressKey(KeyEvent.VK_CONTROL);
+        frameFixture.tree("tree.sTreeCore").clickPath("Amsterdam/Berlin");
+        frameFixture.releaseKey(KeyEvent.VK_CONTROL);
+        // THEN
+        Assertions.assertEquals(2, sTree.getSelection().size());
+        Assertions.assertTrue(sTree.getSelection().contains(amsterdam));
+        Assertions.assertTrue(sTree.getSelection().contains(berlin));
+
+        // WHEN control select
+        frameFixture.pressKey(KeyEvent.VK_CONTROL);
+        frameFixture.tree("tree.sTreeCore").clickPath("Amsterdam/Berlin/Rome");
+        frameFixture.releaseKey(KeyEvent.VK_CONTROL);
+        // THEN
+        Assertions.assertEquals(3, sTree.getSelection().size());
+        Assertions.assertTrue(sTree.getSelection().contains(amsterdam));
+        Assertions.assertTrue(sTree.getSelection().contains(berlin));
+        Assertions.assertTrue(sTree.getSelection().contains(rome));
+
+        // WHEN control select
+        frameFixture.pressKey(KeyEvent.VK_CONTROL);
+        frameFixture.tree("tree.sTreeCore").clickPath("Amsterdam/Berlin/Rome/Amsterdam");
+        frameFixture.releaseKey(KeyEvent.VK_CONTROL);
+        // THEN
+        Assertions.assertEquals(4, sTree.getSelection().size());
+        Assertions.assertTrue(sTree.getSelection().contains(amsterdam));
+        Assertions.assertTrue(sTree.getSelection().contains(berlin));
+        Assertions.assertTrue(sTree.getSelection().contains(rome));
+        Assertions.assertEquals(2, sTree.getSelection().stream().filter(c -> c.equals(amsterdam)).count());
+    }
 }
